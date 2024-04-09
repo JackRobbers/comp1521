57984f95e724f186c3ba2a7e8568ce41d8e7528c
Jack Robbers <jack@2pisoftware.com>
2024-04-09 13:16:09 +1000

    after week 8 tute

--- 24T1/8/fgrep.c
+++ 24T1/8/fgrep.c
@@ -1,6 +1,6 @@
 // search either in stdin or files
 // argv[1] is the string to search for
-// argv[2...] is optional and are filenames
+// argv[2...] is optional and are filenames to look in
 
 #include <stdio.h>
 #include <string.h>
@@ -11,25 +11,23 @@
 void search(FILE *file, char *name, char *search);
 
 int main(int argc, char *argv[]) {
+    // error when argc <= 1;
     if (argc <= 1) {
-        // throw an error if argc <= 1
-        printf("need 1+ arguments\n");
+        printf("need at least 1 argument\n");
     } else if (argc == 2) {
-        // if argc == 2 take input from stdin
-        search(stdin, "stdin", argv[1]);
+        // search in stdin
+        search(stdin, "<stdin>", argv[1]);
     } else {
-        // if argc > 2 read from files.
+        // search in each file
         for (int i = 2; i < argc; i++) {
-            // open each of the files given by argv[2] .. agrv[argc]
             FILE *file = fopen(argv[i], "r");
             if (file == NULL) {
-                printf("error opening file %s: %s\n", argv[i], strerror(errno));
+                // perror(argv[i]);
+                printf("%s: %s\n", argv[1], strerror(errno));
             } else {
-                // run search on them
                 search(file, argv[i], argv[1]);
             }
         }
-
     }
     return 0;
 }
@@ -38,13 +36,10 @@ int main(int argc, char *argv[]) {
 // and print them out.
 void search(FILE *file, char *name, char *search) {
     char line[MAX];
-    int line_number = 1;
-    // loop through each line of the file
+    int line_number = 0;
     while (fgets(line, MAX, file) != NULL) {
-        // check if that line contains search
         if (strstr(line, search) != NULL) {
-            // add the line number when printing lines
-            printf("found a match in file %s on line %d: %s", name, line_number, line);
+            printf("found a match in %s on line %d: %s",name, line_number, line);
         }
         line_number++;
     }
