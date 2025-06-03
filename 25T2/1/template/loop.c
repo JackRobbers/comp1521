5caf3c7bfbbf63e4c53efc10d06f7480e91f0958
Jack Robbers <jack@2pisoftware.com>
2025-06-03 12:42:54 +1000

    start t13a

---
+++ 25T2/1/template/loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int i = 0;
+    while (i < 10)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+    printf("final i was %d", i);
+    return 0;
+}
